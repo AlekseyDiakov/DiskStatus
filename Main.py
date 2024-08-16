af0ee7ccbf20c957c07cf0b13f8eaf3e48075eea
AlekseyDiakov <tornissimo@yandex.ru>
2024-08-16 23:59:34 +0300

    Bug fixed

--- Main.py
+++ Main.py
@@ -24,15 +24,15 @@ total_disk_space_dict = {disk: get_total_disk_space_gb(disk) for disk in disk_dr
 used_disk_space_dict = {disk: get_used_disk_space_gb(disk) for disk in disk_drives}
 free_disk_space_dict = {disk: get_free_disk_space_gb(disk) for disk in disk_drives}
 
-# print(disk_space_dict)
+if __name__ == "__main__":
 
-app = QtWidgets.QApplication(sys.argv)
-winapp = uic.loadUi("TstFrSpc.ui")
+    app = QtWidgets.QApplication(sys.argv)
+    winapp = uic.loadUi("TstFrSpc.ui")
 
-winapp.Btn_total.clicked.connect(lambda: winapp.textBrowser.setText("\n".join(f"Disk {key}: Total space: {value:.2f} Gb" for key, value in total_disk_space_dict.items())))
-winapp.Btn_usage.clicked.connect(lambda: winapp.textBrowser.setText("\n".join(f"Disk {key}: Used space: {value:.2f} Gb" for key, value in used_disk_space_dict.items())))
-winapp.Btn_free.clicked.connect(lambda: winapp.textBrowser.setText("\n".join(f"Disk {key}: Free space: {value:.2f} Gb" for key, value in free_disk_space_dict.items())))
+    winapp.Btn_total.clicked.connect(lambda: winapp.textBrowser.setText("\n".join(f"Disk {key}: Total space: {value:.2f} Gb" for key, value in total_disk_space_dict.items())))
+    winapp.Btn_usage.clicked.connect(lambda: winapp.textBrowser.setText("\n".join(f"Disk {key}: Used space: {value:.2f} Gb" for key, value in used_disk_space_dict.items())))
+    winapp.Btn_free.clicked.connect(lambda: winapp.textBrowser.setText("\n".join(f"Disk {key}: Free space: {value:.2f} Gb" for key, value in free_disk_space_dict.items())))
 
 
-winapp.show()
-sys.exit(app.exec_())
+    winapp.show()
+    sys.exit(app.exec_())
